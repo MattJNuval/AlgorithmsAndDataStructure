822f462517bb0c2dadc3a79778faafe67035707e
Mathew Nuval <nuval.mathewj@gmail.com>
2019-12-28 15:17:48 -0800

    Added insertion sort

--- Sort/src/Driver.java
+++ Sort/src/Driver.java
@@ -1,10 +1,10 @@
 public class Driver {
 
     public static void main(String[] args) {
-        int[] arr = {2, 1, 3, 4, 7, 7, 4, 8, 9, 0};
+        int[] arr = {2,3,5,7,4,1};
 
         Sort s = new Sort();
-        s.mergeSort(arr,0,arr.length-1);
+        s.insertionSort(arr);
 
         for(int i = 0; i < arr.length; i++) {
             System.out.print( arr[i]+", ");
--- Sort/src/Sort.java
+++ Sort/src/Sort.java
@@ -3,7 +3,7 @@ public class Sort {
     public void bubbleSort(int[] data) {
         for(int i = 0; i < data.length; i++) {
             // i was added to adjust the size of the array accordingly
-            for(int j = 0; j < data.length - i-1; j++) {
+            for(int j = 0; j < data.length-i-1; j++) {
                 if(data[j] > data[j+1]) {
                     int temp = data[j];
                     data[j] = data[j+1];
@@ -13,12 +13,38 @@ public class Sort {
         }
     }
 
+    public void selectionSort(int[] data) {
+        for(int i =0; i < data.length; i++) {
+            int min_index = i;
+            for(int j = i+1; j < data.length; j++) {
+                if(data[j] < data[min_index]) {
+                    min_index = j;
+                }
+            }
+            int temp = data[min_index];
+            data[min_index] = data[i];
+            data[i] = temp;
+        }
+    }
+
+    public void insertionSort(int[] arr) {
+        for(int i =0; i < arr.length; i++){
+            for(int j=0;  j < i; j++){
+                if(arr[i] <  arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
     public void quickSort(int[] data, int low, int high) {
         if(low < high) {
             int pi = partition(data, low, high);
 
-            quickSort(data,pi+1, high);
             quickSort(data, low, pi-1);
+            quickSort(data,pi+1, high);
         }
     }
 
@@ -32,14 +58,14 @@ public class Sort {
 
                 int temp = data[i];
                 data[i] = data[j];
-                data[j] = data[i];
+                data[j] = temp;
             }
         }
 
         int temp  = data[i+1];
         data[i+1] = data[high];
         data[high] = temp;
-        return i+1;
+        return (i+1);
     }
 
     public void mergeSort(int[] data, int left, int right) {
